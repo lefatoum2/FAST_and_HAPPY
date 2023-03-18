901f8bde7475b847588743ce8923b04be526a834
lefatoum2 <christianmbarga29@yahoo.fr>
2023-03-18 21:38:59 +0100

    images

---
+++ Insta/routers/post.py
@@ -0,0 +1,41 @@
+import random, string, shutil
+from fastapi import APIRouter, Depends, status, UploadFile, File
+from sqlalchemy.orm import Session
+from routers.schemas import PostBase, PostDisplay
+from db.database import get_db
+from fastapi.exceptions import HTTPException
+from db import db_post
+from typing import List
+router = APIRouter(
+    prefix='/post',
+    tags=['post']
+)
+
+image_url_types = ['absolute', 'relative']
+
+
+@router.post('', response_model=PostDisplay)
+def create(request: PostBase, db: Session = Depends(get_db)):
+    if not request.image_url_type in image_url_types:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                            detail="Le paramètre de image_url_type peut seulement prendre une valeur absolue('absolute') ou relative('relative'). ")
+    return db_post.create(db, request)
+
+
+@router.get('/all', response_model=List[PostDisplay])
+def posts(db: Session = Depends(get_db)):
+    return db_post.get_all(db)
+
+
+@router.post('/image')
+def upload_image(image: UploadFile = File(...)):
+    letters = string.ascii_letters
+    rand_str = ''.join(random.choice(letters) for i in range(6))
+    new = f'_{rand_str}.'
+    filename = new.join(image.filename.rsplit('.',1))
+    path = f'images/{filename}'
+
+    with open(path, "w+b") as buffer:
+        shutil.copyfileobj(image.file, buffer)
+
+    return {'filename': path}
