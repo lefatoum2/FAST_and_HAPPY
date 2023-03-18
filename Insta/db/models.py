56353235ad9be0f5904be8c9280203a9f97685cd
lefatoum2 <christianmbarga29@yahoo.fr>
2023-03-18 19:19:20 +0100

    create post

--- Insta/db/models.py
+++ Insta/db/models.py
@@ -10,3 +10,14 @@ class DbUser(Base):
     username = Column(String)
     email = Column(String)
     password = Column(String)
+    items = relationship('DbPost', back_populates='user')
+
+class DbPost(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True, index=True)
+    image_url = Column(String)
+    image_url_type = Column(String)
+    caption = Column(String)
+    timestamp = Column(DateTime)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship('DbUser', back_populates='items')
