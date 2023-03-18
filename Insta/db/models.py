cab1f93f708e9a6ccf4ccfac4aab8017b2d25e17
lefatoum2 <christianmbarga29@yahoo.fr>
2023-03-18 16:11:50 +0100

    password encryption

--- Insta/db/models.py
+++ Insta/db/models.py
@@ -1,10 +1,12 @@
+from sqlalchemy.sql.schema import ForeignKey
 from .database import Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import relationship
 
 
 class DbUser(Base):
     __tablename__ = 'user'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, index=True)
     username = Column(String)
     email = Column(String)
     password = Column(String)
