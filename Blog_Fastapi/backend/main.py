407c95841264f8efa38a11a9b7f915d5ad9fc312
lefatoum2 <christianmbarga29@yahoo.fr>
2023-03-23 23:34:37 +0100

    api blog

---
+++ Blog_Fastapi/backend/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+import uvicorn
+import models
+from database import engine
+import post
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.include_router(post.router)
+
+models.Base.metadata.create_all(engine)
+
+app.mount('/images', StaticFiles(directory='images'), name='images')
+
+# Acces rigth
+origins = [
+    'http://localhost:3000'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+if __name__ == '__main__':
+    uvicorn.run("main:app", reload=True, port=8000, host="127.0.0.1")
